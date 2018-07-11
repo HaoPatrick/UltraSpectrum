e5843f1bd5a0f8d391188d4707553d26836fc10d
Xiangpeng Hao <3150102255@zju.edu.cn>
2018-07-11 09:22:01 -0700

    aaa

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,13 +1,10 @@
 import { app } from "./firebase";
-
+(async () =>
+    app.firestore().enablePersistence()
+)();
 export async function getAt(index: number = 10) {
     const db = app.firestore();
     const specRef = db.collection("Spectrum");
-    specRef.limit(100).get().then(
-        querySnapshot => {
-            querySnapshot.forEach(doc => {
-                console.log(doc.data());
-            })
-        }
-    );
+    const querySnapshot = await specRef.limit(100).get();
+    console.log(querySnapshot.docs);
 }
