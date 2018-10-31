2a278319279aa19ff2c68c6dbeda8367edf6a9da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-31 04:50:34 +0000

    Deploy HaoPatrick/UltraSpectrum to github.com/HaoPatrick/UltraSpectrum.git:gh-pages

---
+++ precache-manifest.204cb55929ac6580598225d515fc2dc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c12f98ea2a0bedf39170",
+    "url": "/js/chunk-vendors.c12f98ea.js"
+  },
+  {
+    "revision": "091d0ff9165294e25545",
+    "url": "/js/app.091d0ff9.js"
+  },
+  {
+    "revision": "561b0a854a2d1702cfb28a92bdb5fad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "2ef67369847e36dfe737c235e6dae669",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "c12f98ea2a0bedf39170",
+    "url": "/css/chunk-vendors.fad41e8b.css"
+  },
+  {
+    "revision": "091d0ff9165294e25545",
+    "url": "/css/app.fd2820da.css"
+  }
+];
